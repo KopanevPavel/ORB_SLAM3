53ad40583ccd2ef05c571d2e6985803e3baab43e
KopanevPavel <kopanev-pavel-spb@yandex.ru>
2021-04-19 17:30:56 +0300

    Slight fixes

--- Examples/ROS/ORB_SLAM3/src/ros_rgbd.cc
+++ Examples/ROS/ORB_SLAM3/src/ros_rgbd.cc
@@ -77,7 +77,9 @@ int main(int argc, char **argv)
     SLAM.Shutdown();
 
     // Save camera trajectory
-    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory_TUM_Format.txt");
+    SLAM.SaveTrajectoryTUM("FrameTrajectory_TUM_Format.txt");
+    SLAM.SaveTrajectoryKITTI("FrameTrajectory_KITTI_Format.txt");
 
     ros::shutdown();
 
